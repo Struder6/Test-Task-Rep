3f1e4d091e568502263ab96844453bbdbedf6ae8
DostavshikDiscomforta <167789293+Struder6@users.noreply.github.com>
2025-01-22 21:34:06 +0300

    Update mid_task.py

--- mid_task.py
+++ mid_task.py
@@ -1,2 +1,10 @@
 # ЗАДАНИЕ: Поиск дубликатов в массиве
 # Напишите функцию, которая принимает список чисел и возвращает количество повторений каждого элемента.
+def main(lst):
+  nums = []
+  answer = dict()
+  for i in lst:
+    if i not in nums and lst.count(i) > 1:
+      nums.append(i)
+      answer[i] = lst.count(i)
+  
